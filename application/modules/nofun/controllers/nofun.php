0a0336a9545a2dd390c8aebfe1b9ebfd11ac4a6d
babu <toton333@yahoo.com>
2014-10-22 00:17:32 +0530

    Installation and initial setup of modular extensions hmvc

---
+++ application/modules/nofun/controllers/nofun.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Nofun extends MX_Controller {
+
+
+	public function index()
+	{
+		echo 'hello';
+	}
+
+
+	//end of class bracket
+}
+
